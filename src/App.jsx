044fff4aed5ce8099ee372d6d8ca00926a50e4f6
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-03-18 18:43:57 -0400

    added under construction

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
-import React from "react";
-import "./App.css"
+import "./App.css";
+
 
 const App = () => {
   return (
-    <div className="calculator-grid">
-      <div className="output">
-        <div className="previous-operand">123, 456 *</div>
-        <div className="current-operand">234, 342</div>
+    <div className="app-container">
+      <div className="calculator-grid">
+        <div className="output">
+          <div className="previous-operand">123, 456 * 2</div>
+          <div className="current-operand">246, 912</div>
+        </div>
+        <button className="span-two">AC</button>
+        <button>DEL</button>
+        <button>÷</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button className="span-two">=</button>
       </div>
-      <button className="span-two">AC</button>
-      <button>DEL</button>
-      <button>÷</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
-      <button className="span-two">=</button>
+      <div className="under-construction">Currently Under Construction...</div>
     </div>
   );
 };
